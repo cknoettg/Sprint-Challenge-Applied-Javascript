28c93d571a22e96daec9642d251cb8667f870c3b
Corey Knoettgen <cknoettg@yahoo.com>
2020-03-20 22:33:44 -0400

    Saved what I have so far for Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+let myObj2 = axios.get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    //may need to do some chaining here
+    //will call items individually for now, and may loop later
+    let keys = Object.keys(response.data.articles);
+    console.log(keys);
+
+    //sample individual item for reference
+    // response.data.articles.javascript.forEach(item => {
+    //     cards.appendChild(articleCreator(item));
+    // })
+    
+  }) //end then 1
+  .catch(err => {
+    console.log(err);
+  })
+
+function articleCreator(dataObj) {
+  let card = document.createElement("div");
+  let headline = document.createElement("div");
+  let author = document.createElement("div");
+  let imgDiv = document.createElement("div");
+  let img = document.createElement("img");
+  let span = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgDiv.classList.add("img-container");
+
+  //headline.textContent = dataObj.[articles].[subheaders].headline;
+  //img.src = dataObj.[articles].[subheaders].authorPhoto;
+  //span.textContent = "By", dataObj.[articles].[subheaders].authorName;
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgDiv);
+  author.appendChild(span);
+  imgDiv.appendChild(img);
+
+  return card;
+}
+
+let cards = document.querySelector(".cards-container");
